9704e95f4db8741f554c50ab65ce41fcff2b1562
Mitch J <mjacovet@kent.edu>
2025-08-08 09:11:47 -0400

    start using caddy structs for internal config

--- pkg/public_web_server.go
+++ pkg/public_web_server.go
@@ -1,6 +1,7 @@
 package internetgolf
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -8,6 +9,7 @@ import (
 	// ??? these modules appear to register themselves with the main caddy
 	// module as side effects of being imported. is there a better way to do
 	// this?
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 	_ "github.com/caddyserver/caddy/v2/modules/caddyhttp"
 	_ "github.com/caddyserver/caddy/v2/modules/caddyhttp/encode"
 	_ "github.com/caddyserver/caddy/v2/modules/caddyhttp/encode/gzip"
@@ -22,80 +24,94 @@ type PublicWebServer interface {
 
 type CaddyServer struct{}
 
-func getCaddyStaticRouteBoilerplate(publicUrl string, filePath string) string {
-	// this is unfortunately much easier than creating a struct that can handle
-	// all of these object types
-	return fmt.Sprintf(`{
-		"match": [{ "host": ["%s"] }],
-		"handle": [
-		{
-			"handler": "subroute",
-			"routes": [
-			{
-				"handle": [
-				{
-					"handler": "vars",
-					"root": "%s"
-				}
-				]
-			},
-			{
-				"handle": [
-				{
-					"handler": "headers",
-					"response": { "set": { "Cache-Control": ["max-age=0,no-store"] } }
-				}
-				],
-				"match": [{ "path": ["*/"] }]
-			},
-			{
-				"handle": [
-				{
-					"handler": "encode",
-					"encodings": { "gzip": {}, "zstd": {} },
-					"prefer": ["zstd", "gzip"]
-				},
-				{"browse": {"file_limit": 1000}, "handler": "file_server" }
-				]
-			}
-			]
-		}
-		],
-		"terminal": true
-	}`, publicUrl, filePath)
+// TODO: receive the whole deployment object for to take into account its settings
+func getCaddyStaticRouteBoilerplate(publicUrl string, filePath string) caddyhttp.Route {
+	route := caddyhttp.Route{
+		MatcherSetsRaw: caddyhttp.RawMatcherSets{
+			caddy.ModuleMap{"host": ([]byte)(`["` + publicUrl + `"]`)},
+		},
+		// TODO: handlers
+	}
+
+	return route
+
+	// return fmt.Sprintf(`{
+	// 	"match": [{ "host": ["%s"] }],
+	// 	"handle": [
+	// 	{
+	// 		"handler": "subroute",
+	// 		"routes": [
+	// 		{
+	// 			"handle": [
+	// 			{
+	// 				"handler": "vars",
+	// 				"root": "%s"
+	// 			}
+	// 			]
+	// 		},
+	// 		{
+	// 			"handle": [
+	// 			{
+	// 				"handler": "headers",
+	// 				"response": { "set": { "Cache-Control": ["max-age=0,no-store"] } }
+	// 			}
+	// 			],
+	// 			"match": [{ "path": ["*/"] }]
+	// 		},
+	// 		{
+	// 			"handle": [
+	// 			{
+	// 				"handler": "encode",
+	// 				"encodings": { "gzip": {}, "zstd": {} },
+	// 				"prefer": ["zstd", "gzip"]
+	// 			},
+	// 			{"browse": {"file_limit": 1000}, "handler": "file_server" }
+	// 			]
+	// 		}
+	// 		]
+	// 	}
+	// 	],
+	// 	"terminal": true
+	// }`, publicUrl, filePath)
 }
 
 func (c CaddyServer) Deploy(deployments []Deployment) error {
 	fmt.Printf("deploying %+v\n", deployments)
 	// TODO: implement https://caddyserver.com/docs/api#concurrent-config-changes
 
-	var configs []string
+	httpApp := caddyhttp.App{
+		Servers: map[string]*caddyhttp.Server{
+			"internetgolf": {
+				Listen: []string{":80", ":443"},
+				AutoHTTPS: &caddyhttp.AutoHTTPSConfig{
+					// just for local testing...
+					Disabled: true,
+				},
+				Routes: caddyhttp.RouteList{},
+			},
+		},
+	}
 	for _, deployment := range deployments {
 		uriParts := strings.Split(deployment.ResourceUri, "://")
 		if uriParts[0] == "file" {
-			configs = append(configs, getCaddyStaticRouteBoilerplate(deployment.Matcher, uriParts[1]))
+			httpApp.Servers["internetgolf"].Routes = append(
+				httpApp.Servers["internetgolf"].Routes,
+				getCaddyStaticRouteBoilerplate(deployment.Matcher, uriParts[1]),
+			)
 		}
 		// TODO: docker cases
 	}
 
-	config := fmt.Sprintf(`{
-		"apps": {
-			"http": {
-				"servers": {
-					"srv0": {
-						"automatic_https": {"disable": true},
-						"listen": [":443", ":80", ":8989"],
-						"routes": [%s]
-					}
-				}
-			}
-		}
-	}`, strings.Join(configs, ",\n"))
+	httpJson, err := json.Marshal(httpApp)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Print(config + "\n")
+	caddyConfig := caddy.Config{AppsRaw: caddy.ModuleMap{
+		"http": httpJson,
+	}}
 
-	// TODO: call .Run with Config struct instead of calling .Load with JSON soup?
-	err := caddy.Load([]byte(config), false)
+	err = caddy.Run(&caddyConfig)
 	if err != nil {
 		panic(err)
 	}
