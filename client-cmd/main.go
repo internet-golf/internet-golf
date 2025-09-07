c795425c792113b6e7158673f9933f33387cb5c5
Mitch J <mjacovet@kent.edu>
2025-09-07 03:03:11 -0400

    add deploy-content client command

--- client-cmd/main.go
+++ client-cmd/main.go
@@ -5,23 +5,40 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/mholt/archives"
 	"github.com/spf13/cobra"
+	golfsdk "github.com/toBeOfUse/internet-golf/client-sdk"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "golf",
-	Short: "A server",
+	Short: "Deploy stuff to a server",
 }
+
+// TODO: should all the subcommands run the health check and wait for it to work
+// for a few seconds?
+
+var client = golfsdk.NewAPIClient(&golfsdk.Configuration{
+	UserAgent: "InternetGolfClient",
+	Servers: golfsdk.ServerConfigurations{
+		{URL: "http://localhost:8888"},
+	},
+})
 
 func createDeploymentCommand() *cobra.Command {
 
 	var github string
+	var path string
+	var name string
 
 	createDeployment := cobra.Command{
-		Use:     "create-deployment [deployment-url]",
+		Use:     "create-deployment domain",
 		Example: "create-deployment example.com --github repoOwner/repoName",
 		Short:   "Creates a deployment",
-		Args:    cobra.ExactArgs(1),
+		// TODO: actually, allow multiple domains to be specified (but then how
+		// to give them paths? maybe the format should be example.com/path for
+		// the args)
+		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("hello i am creating a deployment")
 			fmt.Printf("the deployment is for the url %s\n", args[0])
@@ -35,33 +52,99 @@ func createDeploymentCommand() *cobra.Command {
 				externalSourceType = "GithubRepo"
 				externalSource = github
 			}
-			client, clientErr := NewClientWithResponses("http://localhost:8888")
-			if clientErr != nil {
-				panic(clientErr.Error())
-			}
-			resp, err := client.PostDeployNewWithResponse(
-				context.TODO(), PostDeployNewJSONRequestBody{
-					Url:                args[0],
+
+			body, resp, respError := client.
+				DefaultAPI.PostDeployNew(context.TODO()).
+				DeploymentCreateInputBody(golfsdk.DeploymentCreateInputBody{
+					Urls:               []golfsdk.Url{{Domain: args[0]}},
 					ExternalSourceType: &externalSourceType,
 					ExternalSource:     &externalSource,
-				},
-			)
-			if err != nil {
-				panic(err.Error())
+					Name:               &name,
+				}).
+				Execute()
+
+			if respError != nil {
+				panic(respError.Error())
 			}
-			fmt.Printf("success: %v\n", resp.JSON200.Success)
+			fmt.Printf("status: %v\n", resp.Status)
+			fmt.Printf("response body: %+v\n", body)
 		},
 	}
 
 	createDeployment.Flags().StringVar(
 		&github, "github", "", "Specify a Github Repo: repoOwner/repoName",
 	)
+	createDeployment.Flags().StringVar(
+		&path, "path", "", "Specify a path, like \"/my-page\"",
+	)
+	createDeployment.Flags().StringVar(
+		&name, "name", "", "Specify a name for the deployment.",
+	)
 
 	return &createDeployment
 }
 
+func deployContentCommand() *cobra.Command {
+	var name string
+	var files string
+
+	deployContent := cobra.Command{
+		Use:  "deploy-content",
+		Args: cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx := context.TODO()
+			files, err := archives.FilesFromDisk(ctx, nil, map[string]string{
+				files: "content",
+			})
+			if err != nil {
+				panic(err.Error())
+			}
+			format := archives.CompressedArchive{
+				Compression: archives.Gz{},
+				Archival:    archives.Tar{},
+			}
+			tempFile, tempFileErr := os.CreateTemp("", "files-to-deploy")
+			if tempFileErr != nil {
+				panic(tempFileErr.Error())
+			}
+			defer os.Remove(tempFile.Name())
+
+			archiveErr := format.Archive(ctx, tempFile, files)
+			if archiveErr != nil {
+				panic(archiveErr.Error())
+			}
+
+			body, resp, respError := client.
+				DefaultAPI.PutDeployFiles(context.TODO()).
+				Name(name).
+				Contents(tempFile).
+				Execute()
+
+			if respError != nil {
+				panic(respError.Error())
+			}
+			fmt.Printf("status: %v\n", resp.Status)
+			fmt.Printf("response body: %+v\n", body)
+
+		},
+	}
+
+	deployContent.Flags().StringVar(
+		&name, "name", "",
+		"Specify the name of the deployment you wish to update.",
+	)
+
+	deployContent.Flags().StringVar(
+		&files, "files", "",
+		"Supply a path to a directory with the content you wish to deploy.",
+	)
+
+	return &deployContent
+}
+
 func main() {
 	rootCmd.AddCommand(createDeploymentCommand())
+	rootCmd.AddCommand(deployContentCommand())
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
